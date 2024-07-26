38c4497800fbbdc6ce1769aa166b4c5d728c15aa
KristiyanIvanov93 <146179725+KristiyanIvanov93@users.noreply.github.com>
2024-07-26 21:57:48 +0300

    add page routes

--- GamesPlay_Resources/client/src/App.jsx
+++ GamesPlay_Resources/client/src/App.jsx
@@ -1,16 +1,32 @@
+import { Routes, Route } from 'react-router-dom';
+
+
 import Header from './components/header/Header';
 import Login from './components/login/Login';
+import Home from './components/home/Home';
+import Register from './components/register/Register';
+import GameCatalog from './components/game-catalog/GameCatalog';
+import GameCreate from './components/game-create/GameCreate';
+
+
 
 function App() {
 
     return (
         <div id="box">
             <Header></Header>
-            <main id="main-content">
 
+            <main id="main-content">
+                <Routes>
+                    <Route path='/' element={<Home />} />
+                    <Route path='/register' element={<Register />} />
+                    <Route path='/login' element={<Login />} />
+                    <Route path='/games' element={<GameCatalog />} />
+                    <Route path='/games/create' element={<GameCreate />} />
+
+                </Routes>
             </main>
 
-            <h1>Working</h1>
         </div>
     );
 }
